c63670f92de0d39f9ee73fa330718d2de5854dc6
arashmodrad <arashmodaresirad@u.boisestate.edu>
2023-01-19 08:43:03 -0700

    some comments

--- retrive_data.py
+++ retrive_data.py
@@ -12,11 +12,15 @@ class DataDownloader:
         self.link = "https://ebfedata.s3.amazonaws.com/"+self.HUC8_code+"_"+self.name+"/"+self.HUC8_code+"_SpatialData.zip"
 
     def downLoad(self):
+        """
+        A function to retrive USGS cross section data
+        """
         # Directories
         geodatabase_download_directory = "GDP_DL"
         Parent_download_directory = "DATA_DL"
         path = Path.cwd()
 
+        # Check for existing paths or creat new ones
         if os.path.exists(os.path.join(path, Parent_download_directory)):
             print("parent path exists")
         else:
@@ -32,21 +36,26 @@ class DataDownloader:
 
         save_GDP_path = os.path.join(save_GDP_path, self.HUC8_code+"_"+self.name)
 
+        # build the url, download and extract the data
         try:   
             read_url = requests.get(self.link)
+            zip_file = zipfile.ZipFile(io.BytesIO(read_url.content))
         except:
             print("invalid url >> cehck servers")
 
-        zip_file = zipfile.ZipFile(io.BytesIO(read_url.content))
-        
+                
         zip_file.extractall(save_GDP_path)
 
         print('Done!')
 
+# A driver class
 class RunDL:
     @staticmethod
     def main(args):
+        # Get HUC8 as input
         HUC8_code = str(input("Enter HUC8 code: ")) #"11130210"
+        
+        # Search for HUC8 watershed names
         with open('HUC8_names.json') as json_data:
             h_data = json.load(json_data)
         HUC8_names = pd.DataFrame(h_data['data'],columns=['huc', 'basin'])
@@ -55,12 +64,15 @@ class RunDL:
         h_series = HUC8_names['basin'].loc[HUC8_names['huc'] == HUC8_code]
         h_name = str(h_series.iloc[0]).replace(" ", "")
         # name = str(input("Enter watershed name code: ")) #"LakeTexoma"
+        
+        # Bulid an instance of DataDownloader object
         data_obg = DataDownloader(HUC8_code, h_name) 
+
+        # Download 
         data_obg.downLoad()
 
 if __name__ == "__main__":
     RunDL.main([])
 
-# from pandas.io.json import json_normalize
 
 
